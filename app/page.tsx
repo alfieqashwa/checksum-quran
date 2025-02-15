ee2e2370a6d1f3f73cddb371e559dd5635d7a7c7
Alfie Qashwa <alfieqashwa@gmail.com>
2025-02-16 00:59:54 +0700

    feat: add surah Al-Fajr:3 as title

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,29 @@ import { quran } from "@quranjs/api";
 
 export default async function Home() {
   const chapters = await quran.v4.chapters.findAll();
+  const alfajr3 = await quran.v4.verses.findByKey("89:3", {
+    fields: {
+      textImlaei: true,
+    },
+    words: true,
+  });
+  console.log(JSON.stringify(alfajr3, null, 2));
+
   return (
     <div className="p-12">
+      <section className="text-center font-bold">
+        <h2 className="text-3xl tracking-wide">{alfajr3.textImlaei}</h2>
+        <article className="flex tracking-wider text-xl mt-2 text-muted-foreground justify-center space-x-1">
+          {alfajr3.words
+            ?.filter((_, i) => i !== 2)
+            .map((word, i) => (
+              <p key={i} className="italic">
+                {word.translation.text}
+              </p>
+            ))}
+        </article>
+        <p className="text-muted-foreground">(Al-Fajr:3)</p>
+      </section>
       <QuranSummary chapters={chapters} />
       <QuranList chapters={chapters} />
     </div>
