ee7b33c09bbd030a3f8cdb40bb2de8a140d6d0ee
Alfie Qashwa <alfieqashwa@gmail.com>
2025-02-20 17:13:54 +0700

    fix: modify github-url

--- app/about/footer.tsx
+++ app/about/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
   <footer className="pt-6 text-right text-sm">
     <a
-      href="https://github.com/alfieqashwa/"
+      href="https://github.com/alfieqashwa/checksum-quran"
       target="_blank"
       rel="noopener"
       className="text-amber-400 underline"
